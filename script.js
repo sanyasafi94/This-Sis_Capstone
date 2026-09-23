d00c2ca67123ef1a0ad68a242e85a840ba0e0ee9
Sanya Safi <sanya.safi@ufl.edu>
2026-09-23 12:09:44 -0500

    added the infomation for some of the products

--- script.js
+++ script.js
@@ -1,26 +1,48 @@
 //Products
 const products = [
+  //Loop to Loop
   {
     id: 1,
-    name: "Printed Cotton Tote Bag",
-    category: "Bags",
-    price: "$22.00",
-    image: "images/handmade.jpg",
+    name: "Rose Cardigan",
+    category: "Wearable, crochet, rose, Loop to Loop",
+    price: "$130 (Price adjusted for size)",
+    image: "images/bluepinkrosesweater.jpeg",
   },
   {
     id: 2,
-    name: "Crochet Coaster Set",
-    category: "Home Goods",
-    price: "$15.00",
+    name: "Cup Cozy",
+    category: "Home, Cup, Cozy, Winter, Loop to Loop",
+    price: "$5",
+    image: "images/greencupcozy.jpeg",
   },
   {
     id: 3,
-    name: "Chunky Crochet Flower Cardigan",
-    category: "Apparel",
-    price: "$65.00",
+    name: "Large Tote Bag",
+    category: "Bag, tote, brown, Loop to Loop",
+    price: "$45",
+    image: "images/browncrochettote.jpeg",
   },
-  { id: 4, name: "Amethyst Bracelet", category: "Jewelry", price: "$20.00" },
-  { id: 5, name: "Sticker Sheet", category: "Stationary", price: "$10.00" },
+  {
+    id: 4,
+    name: "3pc Vase & Trinket tray Set",
+    category: "Ceramic, Eco Resin, Home, Brown, Loop to Loop",
+    price: "$30",
+    image: "images/brownecoresin.jpeg",
+  },
+  {
+    id: 5,
+    name: "Moon Keychain",
+    category: "Keychain, Loop to Loop, moon, yellow, small",
+    price: "$5",
+    image: "images/moonkeychain.jpeg",
+  },
+  {
+    id: 5,
+    name: "Rose Bouquet Keychain",
+    category: "Keychain, Loop to Loop, rose, flower, medium",
+    price: "$10",
+    image: "images/rosebouquetkeychain.jpeg",
+  },
 ];
 
 const productGrid = document.getElementById("productGrid");
@@ -40,7 +62,7 @@ function displayProducts(items) {
     const card = document.createElement("div");
     card.className = "product-card";
     card.innerHTML = `
-    <img src= ${product.image} alt=${product.name} width="500" height"600">
+    <img src= ${product.image} alt=${product.name} width="300" height"400">
       <h3>${product.name}</h3>
       <p class="price">${product.price}</p>
       <button>See item</button>
